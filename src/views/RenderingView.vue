60da475f82d445ae5d0020f3d34ec9d687829288
이용건 <dldydrjs97@openobject.net>
2024-04-02 15:54:43 +0900

    v-if v-show

---
+++ src/views/RenderingView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1 v-if="bRender">bRender의 값이 true일 경우, h1 블록이 화면에 보여지게 됩니다.</h1>
+    <h1 v-else>bRender의 값이 false일 경우, h1 블록이 화면에 보여지게 됩니다.</h1>
+    <h1 v-if="type==='A'">A</h1>
+    <h1 v-else-if="type==='B'"></h1>
+    <h1 v-else>C</h1>
+    <h1 v-show="bRender"> A</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      bRender: true,
+      type: 'G'
+    }
+  }
+}
+</script>
