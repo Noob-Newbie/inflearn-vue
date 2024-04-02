c491ac0a2eff2e9c97944f11edaba4238b9e33d9
이용건 <dldydrjs97@openobject.net>
2024-04-02 14:01:45 +0900

    기본적인 데이터 바인딩

--- src/router/index.js
+++ src/router/index.js
@@ -17,8 +17,8 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/contact',
+    name: 'contact',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -31,6 +31,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact", webpackPrefetch:true */ '../views/BasicView.vue')
+  },
+  {
+    path: '/DataBinding',
+    name: 'DataBinding',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "contact" */ '../views/DataBinding.vue')
+  },
+  {
+    path: '/DataBindingInputText',
+    name: 'DataBindingInputText',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "contact" */ '../views/DataBindingInputText.vue')
   }
 ]
 
---
+++ src/views/DataBindingInputText.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="text" v-model="valueModel">
+    <input type="number" v-model="valueModel2">
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data(){
+      return {
+        valueModel : 'ㅁㄴㅇㄹ',
+        valueModel2 : 13
+      }
+    },
+    setup(){
+    },
+    created() {
+    },
+    mounted() {
+      console.log(this.valueModel)
+      console.log(this.valueModel2)
+    },
+    unmounted() {
+    },
+    methods: {}
+
+  }
+</script>
