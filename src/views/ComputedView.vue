1a986b033e205a391936e7041c14d1d9fbdc784c
이용건 <dldydrjs97@openobject.net>
2024-04-02 17:04:54 +0900

    Computed

---
+++ src/views/ComputedView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" v-model="lNm">
+    <input type="text" v-model="fNm">
+    <h1>hello, {{fullName}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      fNm : '재석',
+      lNm : '유'
+    }
+  },
+  computed: {
+    fullName(){
+      return this.fNm + this.lNm;
+    }
+  },
+  methods: {
+    getFullName (){
+      return this.fNm + this.lNm;
+    }
+  }
+}
+</script>
