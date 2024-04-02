013feb669a54ee3fb31478787a0094d1457245b1
이용건 <dldydrjs97@openobject.net>
2024-04-02 16:55:50 +0900

    이벤트 처리

--- src/router/index.js
+++ src/router/index.js
@@ -55,6 +55,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact" */ '../views/ListBinding.vue')
+  },
+  {
+    path: '/RenderingView',
+    name: 'RenderingView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "contact" */ '../views/RenderingView.vue')
+  },
+  {
+    path: '/CounterView',
+    name: 'CounterView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "contact" */ '../views/CounterView.vue')
   }
 ]
 
---
+++ src/views/CounterView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+<!--    <button type="button" @click="one(), two()">add1</button>
+    <p>count is : {{counter}}</p>
+    <select v-model="city" @change="changeSelect">
+      <option value="서울">서울</option>
+      <option value="부산">부산</option>
+      <option value="제주">제주</option>
+    </select>-->
+    <input type="text" v-model="textValue" @keyup.enter="showValue">
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      counter : 0,
+      city: "",
+      textValue: ""
+    }
+  },
+  methods : {
+    one(){
+      alert('one')
+    },
+    two(){
+      alert('two')
+    },
+    changeSelect(){
+      alert(this.city)
+    },
+    showValue(){
+      alert(this.textValue)
+    }
+  }
+}
+</script>
