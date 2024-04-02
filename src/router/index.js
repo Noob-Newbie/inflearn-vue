a7af04c664cc2b65553f74c95dbb5a5c6d24aa06
이용건 <dldydrjs97@openobject.net>
2024-04-02 17:58:05 +0900

    Watch

--- src/router/index.js
+++ src/router/index.js
@@ -79,6 +79,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact" */ '../views/ComputedView.vue')
+  },
+  {
+    path: '/WatchView',
+    name: 'WatchView',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "contact" */ '../views/WatchView.vue')
   }
 ]
 
---
+++ src/views/WatchView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <input type="number" v-model.number="x1">
+    <input type="number" v-model.number="x2">
+<!--    <button type="button" @click="plusNumber">계</button>-->
+    <input type="number" v-model.number="y">
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      x1 : 0,
+      x2 : 0,
+      y : 0
+    }
+  },
+  watch : {
+    x1(){
+      this.y = this.x1 + this.x2
+    },
+    x2(){
+      this.y = this.x1 + this.x2
+    }
+  },
+  setup(){},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    plusNumber(){
+      this.y = this.x1 + this.x2
+    }
+  }
+}
+</script>
