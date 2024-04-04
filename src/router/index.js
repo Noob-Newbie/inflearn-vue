376e8a2661b9bb2311f62a7de4038d610d5a8763
이용건 <dldydrjs97@openobject.net>
2024-04-04 16:52:10 +0900

    외부 package 적용 및 MockServer연동

--- src/router/index.js
+++ src/router/index.js
@@ -19,90 +19,62 @@ const routes = [
   {
     path: '/contact',
     name: 'contact',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact", webpackPrefetch:true */ '../views/basic/ContactView.vue')
   },
   {
     path: '/basic',
     name: 'basic',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact", webpackPrefetch:true */ '../views/basic/BasicView.vue')
   },
   {
     path: '/DataBinding',
     name: 'DataBinding',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact" */ '../views/basic/DataBinding.vue')
   },
   {
     path: '/DataBindingInputText',
     name: 'DataBindingInputText',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact" */ '../views/basic/DataBindingInputText.vue')
   },
   {
     path: '/ListBinding',
     name: 'ListBinding',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact" */ '../views/basic/ListBinding.vue')
   },
   {
     path: '/RenderingView',
     name: 'RenderingView',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact" */ '../views/basic/RenderingView.vue')
   },
   {
     path: '/CounterView',
     name: 'CounterView',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact" */ '../views/basic/CounterView.vue')
   },
   {
     path: '/ComputedView',
     name: 'ComputedView',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact" */ '../views/basic/ComputedView.vue')
   },
   {
     path: '/WatchView',
     name: 'WatchView',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact" */ '../views/basic/WatchView.vue')
   },
   {
     path: '/SearchView',
     name: 'SearchView',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact" */ '../views/basic/SearchView.vue')
   },
   {
     path: '/InsertView',
     name: 'InsertView',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact" */ '../views/basic/InsertView.vue')
+  },
+  {
+    path: '/ServerData',
+    name: 'ServerData',
+    component: () => import(/* webpackChunkName: "contact" */ '../views/basic/ServerData.vue')
   }
 ]
 
