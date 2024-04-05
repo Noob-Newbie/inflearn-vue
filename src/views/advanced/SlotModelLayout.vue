56dd1253937eb83e25c4b2ca987f92574f440bed
Noob-Newbie <ygzzang97@gmail.com>
2024-04-05 18:21:38 +0900

    슬롯 모달 수정중

---
+++ src/views/advanced/SlotModelLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="modal-container">
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  },
+  setup(){},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+
+  }
+}
+</script>
---
+++ src/views/advanced/SlotUseModalLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <modal-layout>
+    <template v-slot:header>
+      <h1>타이틀</h1>
+    </template>
+    <template v-slot:default>
+      <p>팝업컨텐츠1</p>
+      <p>팝업컨텐츠2</p>
+    </template>
+    <template v-slot:footer>
+
+  </modal-layout>
+</template>
+<script setup lang="ts">
+</script>
